8c54bc7e1955e4b8383dd51a4dd7a74862b954f5
feasible <feasible.creative@gmail.com>
2022-08-17 19:12:53 -0400

    develop JavaScript pseudocode, initial code with minor bugs, update CSS aesthetics, add images to img folder

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log('test');
+//User starts with $X
+//User Inputs bet amount
+//Click roll
+//Return 3 random options "7, BAR(x3), BAR, Free Play, etc."
+//Each play, subtrack bet amount
+
+// console.log(userBet - points)
+
+
+function slotSpin(){
+  // let userBet = parseInt(prompt("Place your bet!"));
+  // // console.log(typeof(userBet))
+for (i = 0; i < 3; i++){
+
+  function getSpinResult(spinOptions = ['7', 'BAR', 'Free Play']){ 
+      
+      return spinResult = spinOptions[Math.floor(Math.random()*spinOptions.length)];
+
+  }
+ 
+  getSpinResult();
+
+  
+  // let element = document.createElement('div');
+  // element.innerText = spinResult;
+  // element.setAttribute('class', 'casino');
+  // document.body.appendChild(element);
+
+
+
+imgArray = ['img_1.jpg', 'img_2.jpg', 'img_3.jpg']
+
+function getRandomImg(){ 
+
+  randomIndex = Math.floor(Math.random()* imgArray.length);
+
+  selectedImg = imgArray[randomIndex]
+
+  document.getElementById('slotResult').src = `./img/${selectedImg}`
+
+  slotResult.className = 'casino';
+
+
+
+}
+
+
+
+getRandomImg();
+
+  
+
+
+
+
+
+  
+};
+
+};
+
+slotSpin(4);
