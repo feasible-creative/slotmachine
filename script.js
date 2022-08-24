3ae0cd1e8d1d1239750f7b251668b1694cf409c8
feasible <feasible.creative@gmail.com>
2022-08-23 20:56:56 -0400

    create result array

--- script.js
+++ script.js
@@ -7,25 +7,28 @@
 // console.log(userBet - points)
 
 
+
+resultArray = [];
+
+imgArray = ['img_1.jpg', 'img_2.jpg', 'img_3.jpg'];
+
+let dict = {'img_1.jpg': 1, 'img_2.jpg': 2, 'img_3.jpg': 3}
+
+
+
 function slotSpin(){
     
-// for (i = 0; i < 1; i++){
+    let button = document.getElementById('slot-button');
+    button.addEventListener('click', function getRandomImg(){ 
+    
+    randomIndex = Math.floor(Math.random()* imgArray.length);
+    selectedImg = imgArray[randomIndex]
 
-  
-imgArray = ['img_1.jpg', 'img_2.jpg', 'img_3.jpg']
-
-
-
-let button = document.getElementById('slot-button');
-button.addEventListener('click', function getRandomImg(){ 
+    document.getElementById('slotResult').src = `./img/${selectedImg}`
  
-randomIndex = Math.floor(Math.random()* imgArray.length);
-selectedImg = imgArray[randomIndex]
+    resultArray[0] = dict[selectedImg];
 
-document.getElementById('slotResult').src = `./img/${selectedImg}`
-slotResult.className = 'casino'}
-  
-   
+  } 
   
   );
 
@@ -36,10 +39,11 @@ slotResult.className = 'casino'}
     selectedImg = imgArray[randomIndex]
   
     document.getElementById('slotResult2').src = `./img/${selectedImg}`
-    slotResult.className = 'casino'}
     
-     
-    
+    resultArray[1] = dict[selectedImg];
+
+  
+  }
     );
   
     let button3 = document.getElementById('slot-button');
@@ -49,29 +53,82 @@ slotResult.className = 'casino'}
     selectedImg = imgArray[randomIndex]
     
     document.getElementById('slotResult3').src = `./img/${selectedImg}`
-    slotResult.className = 'casino'}
-      
-       
-      
-      );
     
-    };
+    resultArray[2] = dict[selectedImg];
+
+    // console.log(dict[selectedImg]);
+    
+ }
   
+
+
+      );
+
+      console.log(resultArray);  
+     
+    };
 
 
 slotSpin();
   
   
-  // console.log(slotResult3, slotResult2);
 
-// if(slotResult3.casino === slotResult2.casino){
-//   alert('epic');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// newArray = [1, 2, 3]
+
+// let result = document.querySelector('result')
+
+
+// for (let i = 0; i <= newArray.length - 1; i++){
+ 
+//   randomIndex = Math.floor(Math.random()* newArray.length);
+//   selectedImg = newArray[randomIndex]
+  
+//   document.getElementById('result') =  `${selectedImg}`;
+
+
+//   console.log(newArray[i]);
+
 // }
-// else{
-//   console.log('fix')
-// }
 
 
 
 
 
+
+
+
+
+
+
+// arrayCheck = ['cherries', '7', 'bar']
+
+// for (let i = 0; i <= arrayCheck.length - 1; i++){
+//   randomIndex = Math.floor(Math.random() * arrayCheck.length);
+// randomNumber = arrayCheck[randomIndex];
+//   console.log(randomNumber );
+
+//   if (randomNumber + [1] === 'cherries'){
+//     console.log('nice cherries');
+  
+//  };
+
+
+//  }
